84bddba8ee57ef7d0f0a237179d6f83a21d2a402
tabriznh <88050551+tabriznh@users.noreply.github.com>
2021-10-20 19:14:53 +0400

    hmw

---
+++ hmw/js/script.js
@@ -0,0 +1,46 @@
+// let num1 = prompt("Enter th first number");
+// let num2 = prompt("Enter th second number");
+
+// alert(positiveNegative(num1, num2));
+
+// function positiveNegative(num1, num2){
+//     if (num1<0 && num2>0 || num1>0 && num2<0) {
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+
+// ******************************************************************************
+
+
+// Task 1 - Rectangle Area
+
+// function rectArea(a, b) {
+//     return a * b;
+// }
+
+// console.log(rectArea(2, 3));
+
+
+// ******************************************************************************
+
+
+// Task 2 Alphabetical Order
+
+// function alphabeticalOrder(text){
+//     return text.split('').sort().join('');
+// }
+
+// console.log(alphabeticalOrder("webmaster"));
+
+// ******************************************************************************
+
+// Task 3 Reverse a number
+
+// function reverseNumber(num){
+//     return parseFloat(num.toString().split('').reverse().join(''));
+// }
+
+// console.log(reverseNumber(1234567));
